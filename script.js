08666c3b1f1629e98d226874a6ffcbb97589fbaa
deepanshu-dhaka <dhakadeepanshu30@gmail.com>
2026-06-07 07:30:24 +0000

    Implement the basic features of the project

--- script.js
+++ script.js
@@ -9,6 +9,54 @@ for (let x = 0; x < 256; x++) {
     squareDiv.classList.add("squareDiv")
     squareDiv.style.width = "20px";
     squareDiv.style.height = "20px";
-    squareDiv.style.border = "2px solid black"
+    squareDiv.style.outline = "2px solid black"
+
     container.appendChild(squareDiv);
+}
+
+let sqaureDivs = document.querySelectorAll(".squareDiv")
+
+sqaureDivs.forEach(function (item) {
+    item.addEventListener("mouseenter", () => item.style.backgroundColor = "black")
+})
+
+let sizeButton = document.querySelector(".size-button");
+sizeButton.addEventListener("click", getSize)
+
+
+function getSize() {
+    let noOfSquareDivsPerSide = +prompt("Enter the no of squares you want per side of the grid:");
+    console.log(typeof noOfSquareDivsPerSide);
+    makeSqaureGrid(noOfSquareDivsPerSide);
+}
+
+function makeSqaureGrid(noOfSquareDivsPerSide) {
+    let totalNoOfSqaureDivs = noOfSquareDivsPerSide * noOfSquareDivsPerSide;
+    let containerWidth = noOfSquareDivsPerSide * 20;
+    let containerHeight = noOfSquareDivsPerSide * 20;
+    container.style.width = `${containerWidth}px`;
+    container.style.width = `${containerHeight}px`;
+    removeExistingGrid()
+    for (let x = 0; x < totalNoOfSqaureDivs; x++) {
+        let squareDiv = document.createElement("div");
+        squareDiv.classList.add("squareDiv")
+        squareDiv.style.width = "20px";
+        squareDiv.style.height = "20px";
+        squareDiv.style.outline = "2px solid black"
+
+        container.appendChild(squareDiv);
+    }
+    let sqaureDivs = document.querySelectorAll(".squareDiv")
+
+    sqaureDivs.forEach(function (item) {
+        item.addEventListener("mouseenter", () => item.style.backgroundColor = "black")
+    })
+}
+
+function removeExistingGrid() {
+    let sqaureDivs = document.querySelectorAll(".squareDiv")
+
+    sqaureDivs.forEach(function (item) {
+        container.removeChild(item);
+    })
 }
